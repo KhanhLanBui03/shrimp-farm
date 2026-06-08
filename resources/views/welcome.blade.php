8f2ee53b181a3aff44291648cf13e9cd46647e6d
buikhanhlan1410 <buikhanhlan1410@gmail.com>
2026-06-08 15:05:52 +0700

    completed setup project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html >
+<head>
+    @vite(['resources/css/app.css'])
+</head>
+
+<body class="bg-gray-100">
+
+    <div class="flex items-center justify-center h-screen">
+
+        <div class="bg-white p-10 rounded-xl shadow-lg">
+
+            <h1 class="text-4xl font-bold text-blue-500">
+                Laravel + Tailwind
+            </h1>
+
+            <button
+                class="mt-5 bg-blue-500 text-white px-4 py-2 rounded-lg"
+            >
+                Login
+            </button>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
